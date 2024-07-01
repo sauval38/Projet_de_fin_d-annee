f00570debc59daaf8e23db654012b032f6866f22
Sauval Charles <boubou601@live.fr>
2024-07-01 11:16:58 +0200

    css_games

--- App/views/GamesViews.php
+++ App/views/GamesViews.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace Views;
 
 class GamesViews {
     public function list($games) {
         echo '<h1>LISTE DES JEUX</h1>';
+        echo '<div class="games-container">';
         foreach ($games as $game) {
-            echo '<a href="">' . $game['title_article'] . '</a>';
-            echo '<p>'. $game['description_article'].'</p>';
+            echo '<div class="game-card">';
+            echo '<a href="#">' . $game['title_article'] . '</a>';
+            echo '<p>'. $game['description_article'] .'</p>';
             echo '<img src="' . $game['path'] . '/' . $game['image_article'] . '">';
+            echo '</div>';
         }
+        echo '</div>';
     }
-}   
+}
